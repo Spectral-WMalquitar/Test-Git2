90972f9feb3578a52f760e49a29099dbec97ccef
mrlatecomer95 <RiptTide>
2015-11-04 13:42:38 +0800

    Change form
    frmMain

---
+++ frmMain.vb
@@ -0,0 +1,6 @@
+﻿Public Class frmMain 
+
+    Private Sub BarButtonItem1_ItemClick(sender As System.Object, e As DevExpress.XtraBars.ItemClickEventArgs) Handles BarButtonItem1.ItemClick
+        MsgBox("Clicked1")
+    End Sub
+End Class
