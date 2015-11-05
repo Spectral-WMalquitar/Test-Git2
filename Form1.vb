1a6e9a9bb736851f178aed7295dc3f65a0e07525
Welly Malquitar <wmalquitar23@gmail.com>
2015-11-05 15:47:36 +0800

    Transfer to VHD Source Code

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        MsgBox("Clicked")
+
+        MsgBox("New Clicked")
+
+    End Sub
+End Class
